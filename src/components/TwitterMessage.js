5ea3c7df17aec18bb6884c0f06eccbdc679b5149
Won Kim <wonsae.kim@gmail.com>
2019-06-26 16:15:59 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {Component} from "react";
 
-class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+class TwitterMessage extends Component {
+  state = {
+    message: '',
+    totalChars: 0
+  }
 
-    this.state = {};
+  calculateCharsLeft = (e) => {
+    let num = this.state.totalChars;
+    this.setState({
+      message: e.target.value,
+      totalChars: num + 1
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.calculateCharsLeft} />
+        {this.props.maxChars - this.state.totalChars} characters left
       </div>
     );
   }
